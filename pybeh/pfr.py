02d89bb086f5186ed457f251f46bc853052a55c9
jpazdera <jpazdera@haverford.edu>
2019-04-19 16:02:48 -0400

    PFR is now just a wrapper for PNR where n=0.

--- pybeh/pfr.py
+++ pybeh/pfr.py
@@ -1,5 +1,5 @@
-from __future__ import division
 import numpy as np
+from pybeh.pnr import pnr
 
 def pfr(recalls, subjects, listLength):
     """"
@@ -27,24 +27,4 @@ def pfr(recalls, subjects, listLength):
         p_recalls:  a matrix of probablities.  Its columns are indexed by
                     serial position and its rows are indexed by subject.
     """
-    if recalls is None:
-        raise Exception('You must pass a recalls matrix.')
-    elif subjects is None:
-        raise Exception('You must pass a subjects vector.')
-    elif listLength is None:
-        raise Exception('You must pass a list length.')
-    elif len(recalls) != len(subjects):
-        raise Exception('recalls matrix must have the same number of rows as subjects.')
-
-    subject = np.unique(subjects)
-    result = [[0] * listLength for count in range(len(subject))]
-    for subject_index in range(len(subject)):
-        count = 0
-        for subj in range(len(subjects)):
-            if subjects[subj] == subject[subject_index]:
-                if recalls[subj][0] > 0 and recalls[subj][0] < 1 + listLength:
-                    result[subject_index][recalls[subj][0] - 1] += 1
-                count += 1
-        for index in range(listLength):
-            result[subject_index][index] /= count
-    return result
+    return pnr(recalls, subjects, listLength, 0)
