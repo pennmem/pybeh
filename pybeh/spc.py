1b008211d20a91f12ab7f2f99baf143aeea6bccf
jpazdera <jpazdera@haverford.edu>
2019-05-03 12:23:57 -0400

    Added ability to calculate SPCs by start position.

--- pybeh/spc.py
+++ pybeh/spc.py
@@ -2,56 +2,49 @@ from __future__ import division
 import numpy as np
 
 
-def spc(recalls=None, subjects=None, listLength=None):
+def spc(recalls, subjects, listLength, start_position=None):
     """
-    SPC   Serial position curve (recall probability by serial position).
+    Serial position curve (recall probability by serial position).
 
-    p_recall = spc(recalls, subjects, listLength)
-
-    INPUTS:
-        recalls:    a matrix whose elements are serial positions of recalled
-                    items. The rows of this matrix should represent recalls
-                    made by a single subject on a single trial.
-
-        subjects:   a 1D array which indexes the rows of the recalls matrix
-                    with a subject number (or other identifier). That is,
-                    the recall trials of subject S should be located in
-                    recalls[subjects==S, :]
-
-        listLength: a scalar indicating the number of serial positions in the
-                    presented lists. serial positions are assumed to run
-                    from [1, 2, ..., list_length].
-
-
-    OUTPUTS:
-        p_recall:  a matrix of probablities.  Its columns are indexed by
-                   serial position and its rows are indexed by subject.
+    :param recalls: A trials x items matrix whose elements are the serial positions of the items recalled on each trial.
+        Item (i, j) should therefore be the serial position of the jth item recalled on trial i.
+    :param subjects: A 1D array indicating which subject (or other identifier) produced the data from each row of the
+        recalls matrix, i.e., recall trials from subject S should be located in recalls[subjects == S, :].
+    :param listLength: A scalar indicating the length of presented lists. Serial positions are assumed to range from
+        1 to listLength (inclusive).
+    :param start_position: (Optional) If an integer, only include trials where recall was initiated from that serial
+        position. If a list of integers is provided, only include trials where recall was initiated from any of the
+        provided serial positions. For example, start_positions=1 will produce a serial position curve for trials where
+        recall began from the first list item, and start_positions=[1, 2, 3] will produce an SPC for trials where recall
+        began from any of the first three items.
+    :return: A 2D numpy array where each row is the average SPC from one subject. Rows will match the subject order
+        produced by np.unique(subjects).
     """
-    if recalls is None:
-        raise Exception('You must pass a recalls matrix.')
-    elif subjects is None:
-        raise Exception('You must pass a subjects vector.')
-    elif listLength is None:
-        raise Exception('You must pass a list length.')
-    elif len(recalls) != len(subjects):
-        raise Exception('recalls matrix must have the same number of rows as subjects.')
+    if len(recalls) != len(subjects):
+        raise Exception('The recalls matrix must have the same number of rows as subjects.')
+    if isinstance(start_position, int):
+        start_position = [start_position]
 
     # Convert inputs to numpy arrays if they are not already
     recalls = np.array(recalls)
     subjects = np.array(subjects)
-    # Get a list of unique subjects
+    # Get list of unique subjects
     usub = np.unique(subjects)
-    # Create list of all serial positions (starting with 1)
+    # Create list of all possible serial positions (from 1 through list length)
     positions = np.arange(1, listLength+1)
-    # We will return one SPC for each unique subject
-    result = np.zeros((len(usub), listLength))
 
+    # Calculate average SPC for each unique subject
+    result = np.full((len(usub), listLength), np.nan)
     for i, subj in enumerate(usub):
         # Select only the trials from the current subject
-        subj_recalls = recalls[subjects == subj]
-        # Create a "recalled" matrix of ones and zeroes indicating whether each presented item was correctly recalled
-        subj_recalled = np.array([np.isin(positions, trial_data) for trial_data in subj_recalls])
-        # Calculate the subject's SPC as the fraction of trials on which they recalled each serial position's item
-        result[i, :] = subj_recalled.mean(axis=0)
+        subj_recalls = recalls[(subjects == subj)]
+        # If filtering by recall start position, select only trials that match
+        if hasattr(start_position, __iter__):
+            subj_recalls = subj_recalls[np.isin(subj_recalls[0], start_position), :]
+        if len(subj_recalls) > 0:
+            # Create a matrix of ones and zeroes indicating whether each presented item was correctly recalled
+            subj_recalled = np.array([np.isin(positions, trial_data) for trial_data in subj_recalls])
+            # Calculate the subject's SPC as the fraction of trials on which they recalled each serial position's item
+            result[i, :] = subj_recalled.mean(axis=0)
 
     return result
